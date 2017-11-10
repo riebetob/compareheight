6756f7de0bf8c9a645136059278c4fd330d70304
riebetob <Tobias.Riebe@campus.lmu.de>
2017-11-10 15:25:03 +0100

    fixed issues of comments and variable names

--- R/checkHeight.R
+++ R/checkHeight.R
@@ -14,12 +14,12 @@ checkHeight <- function(students.input = students, sex.specific = TRUE){
   #Check if the sex specific height difference or the difference to the whole population should be calculated
   if(sex.specific == TRUE){
     #Calculate the gender specific means
-    women_mean_height = as.numeric(students %>%
+    women_mean_height = as.numeric(students.input %>%
                                      group_by(sex) %>%
                                      summarise(mean(height)) %>%
                                      filter(sex == "F") %>%
                                      select("mean(height)"))
-    men_mean_height = as.numeric(students %>%
+    men_mean_height = as.numeric(students.input %>%
                                    group_by(sex) %>%
                                    summarise(mean(height)) %>%
                                    filter(sex == "M") %>%
@@ -27,7 +27,7 @@ checkHeight <- function(students.input = students, sex.specific = TRUE){
     #initialize an empty vector to save the genderspecific height differnces
     height_vector = c()
     #apply a function to the rows of the input dataframe
-    height_vector = apply(students, MARGIN = 1,
+    height_vector = apply(students.input, MARGIN = 1,
                           FUN = function(student){
                             #substract the gender specific means from the individuals to get height differnces
                             (if (student["sex"] == "M") men_mean_height - as.numeric(student["height"])
@@ -35,21 +35,21 @@ checkHeight <- function(students.input = students, sex.specific = TRUE){
                           } )
     #create the final dataframe containing name od the students and the height differnces
     #multiple height differences by 100 to get values in cm
-    result.frame = data.frame("name" = students$name, "sexspec_height_diff" = height_vector*100)
+    result.frame = data.frame("name" = students.input$name, "sexspec_height_diff" = height_vector*100)
   } else{
       #initialize an empty vector to save the height differnces
       height_vector = c()
       #calculate the mean height of the whole population
       mean_height = mean(students.input$height)
       #apply a function to the rows of the input dataframe
-      height_vector = apply(students, MARGIN = 1,
+      height_vector = apply(students.input, MARGIN = 1,
                             FUN = function(student){
                               #substract the gender specific means from the individuals to get height differnces
                               mean_height - as.numeric(student["height"])
                             } )
       #create the final dataframe containing name od the students and the height differnces
       #multiple height differences by 100 to get values in cm
-      result.frame = data.frame("name" = students$name, "sexspec_height_diff" = height_vector*100)
+      result.frame = data.frame("name" = students.input$name, "sexspec_height_diff" = height_vector*100)
   }
   #return the dataframe
   return(result.frame)
--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,7 @@
-mean <- function(input){
-  round(input, digits = 3)
+#'Function for calculating mean and rounding on three decimals
+#'@param values A vector with values
+#'@return The mean rounded on three decimals
 
+mean = function(values){
+  round(sum(values)/length(values),3)
 }
