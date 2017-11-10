7e04f2985e3b0895b9f22a83ed7ace16f551b127
riebetob <33148270+riebetob@users.noreply.github.com>
2017-11-10 15:34:07 +0100

    export function

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,8 @@
+
 #'Function for calculating mean and rounding on three decimals
 #'@param values A vector with values
 #'@return The mean rounded on three decimals
+#'@export
 
 mean = function(values){
   round(sum(values)/length(values),3)
