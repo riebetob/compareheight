23ad2c89c59974b59da06978bd047e2f73cc85c3
riebetob <Tobias.Riebe@campus.lmu.de>
2017-11-13 12:09:06 +0100

    Added checks for utils funtion

--- R/utils.R
+++ R/utils.R
@@ -6,6 +6,6 @@
 mean = function(values){
   #Checks if values are numeric and if the vector at least contains one value for
   #denominator not being 0 later. No missing values allowed
-  assert(checkNumeric(value, any.missing = FALSE, min.len = 1))
+  assert(checkNumeric(values, any.missing = FALSE, min.len = 1))
   round(sum(values)/length(values),3)
 }
